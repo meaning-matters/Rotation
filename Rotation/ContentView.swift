82043b7ec482dd5d0f6557db7a9416fc7857591b
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-10-12 11:20:34 +0200

    Initial Commit

---
+++ Rotation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rotation
+//
+//  Created by Cornelis van der Bent on 12/10/2019.
+//  Copyright © 2019 Meaning Matters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
