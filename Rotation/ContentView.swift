4f948668e8e230b5ad87939af643c043dffb6ed5
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-11-12 15:30:53 +0100

    Fix iPad UI layout - version 1.1

--- Rotation/ContentView.swift
+++ Rotation/ContentView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 import Combine
 
-struct ContentView: View
+struct ContentView : View
 {
     private let startAngle: Double = 135
     @State private var endAngle: Double = 135
@@ -30,63 +30,76 @@ struct ContentView: View
             Color(white: 0.3)
                 .edgesIgnoringSafeArea(.all)
 
-            Circle().padding()
-
-            Text(String(format: "%.0f", (endAngle - startAngle) / 360.0 / sensitivity * 100.0))
-                .foregroundColor(.white)
-                .font(.system(size: 100))
-                .fontWeight(.ultraLight)
-
             GeometryReader
-            { geometry in
-                Text("Averaged 3-Axis Device Rotation")
-                    .foregroundColor(.white)
-                    .font(.system(size: 40))
-                    .multilineTextAlignment(.center)
-                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                    .padding()
+            {  geometry in
+                ZStack
+                {
+                    Color(white: 0.3)
+                        .edgesIgnoringSafeArea(.all)
 
-                Arc(startAngle: self.startAngle,
-                    endAngle: self.$staticEndAngle,
-                    center: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2),
-                    radius: geometry.size.width / 2 - 60)
-                    .fill(Color(white: 0.1))
+                    Circle().padding()
 
-                Arc(startAngle: self.startAngle,
-                    endAngle: self.$clippedEndAngle,
-                    center: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2),
-                    radius: geometry.size.width / 2 - 60)
-                    .fill(AngularGradient(gradient: self.gradient,
-                                          center: .center,
-                                          startAngle: .degrees(self.startAngle),
-                                          endAngle: .degrees(self.startAngle + 270)))
-                    .onReceive((UIApplication.shared.delegate as! AppDelegate).subject)
-                    { factor in
-                        self.endAngle = 360 * factor * self.sensitivity + self.startAngle
-                        self.clippedEndAngle = min(360 * factor * self.sensitivity, 270) + self.startAngle
-                        self.sensitivity -= max((self.endAngle - self.clippedEndAngle) / 360 * factor, 0)
+                    GeometryReader
+                    { geometry in
+                        Text(String(format: "%.0f", (self.endAngle - self.startAngle) / 360.0 / self.sensitivity * 100.0))
+                            .foregroundColor(.white)
+                            .font(.system(size: 100 * (geometry.size.width / 350)))
+                            .fontWeight(.ultraLight)
                     }
 
-                VStack
-                {
-                    Text("Sensitivity")
-                        .font(.system(size: 12))
-                        .foregroundColor(.white)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                        .offset(x: 0, y: 10)
-                    Slider(value: self.$sensitivity, in: 1.0...3.0)
-                        .padding(.horizontal)
+                    GeometryReader
+                    { geometry in
+                        Text("Averaged 3-Axis Device Rotation")
+                            .foregroundColor(.white)
+                            .font(.system(size: 40))
+                            .multilineTextAlignment(.center)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                            .padding()
+
+                        Arc(startAngle: self.startAngle,
+                            endAngle: self.$staticEndAngle,
+                            center: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2),
+                            radius: geometry.size.width / 2 - 60)
+                            .fill(Color(white: 0.1))
+
+                        Arc(startAngle: self.startAngle,
+                            endAngle: self.$clippedEndAngle,
+                            center: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2),
+                            radius: geometry.size.width / 2 - 60)
+                            .fill(AngularGradient(gradient: self.gradient,
+                                                  center: .center,
+                                                  startAngle: .degrees(self.startAngle),
+                                                  endAngle: .degrees(self.startAngle + 270)))
+                            .onReceive((UIApplication.shared.delegate as! AppDelegate).subject)
+                            { factor in
+                                self.endAngle = 360 * factor * self.sensitivity + self.startAngle
+                                self.clippedEndAngle = min(360 * factor * self.sensitivity, 270) + self.startAngle
+                                self.sensitivity -= max((self.endAngle - self.clippedEndAngle) / 360 * factor, 0)
+                            }
+
+                        VStack
+                        {
+                            Text("Sensitivity")
+                                .font(.system(size: 12))
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                                .offset(x: 0, y: 10)
+                            Slider(value: self.$sensitivity, in: 1.0...3.0)
+                                .padding(.horizontal)
+                        }
+                        .offset(x: 0, y: CGFloat(0.75) * geometry.size.height +
+                                         CGFloat(0.25) * (geometry.size.width + CGFloat(2 * 20)) -
+                                         CGFloat(69 / 2) - CGFloat(15))
+                        // ^^^ Above offset vertically centers the VStack in the space below the circle.
+                        //     20 is the common iOS padding() applied to both sides of the outer Circle.
+                        //     69 is estimated height of this VStack with one slider (checked with screenshot).
+                        //     15 is the estimated space above text and border of VStack.
+                    }
                 }
-                .offset(x: 0, y: CGFloat(0.75) * geometry.size.height +
-                                 CGFloat(0.25) * (geometry.size.width + CGFloat(2 * 20)) -
-                                 CGFloat(69 / 2) - CGFloat(15))
-                // ^^^ Above offset vertically centers the VStack in the space below the circle.
-                //     20 is the common iOS padding() applied to both sides of the outer Circle.
-                //     69 is estimated height of this VStack with one slider (checked with screenshot).
-                //     15 is the estimated space above text and border of VStack.
+                .frame(width: min(geometry.size.width, geometry.size.height * 0.75), height: geometry.size.height, alignment: .center)
+                .statusBar(hidden: true)
             }
         }
-        .statusBar(hidden: true)
     }
 }
 
